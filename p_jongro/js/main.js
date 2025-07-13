83e4e348e35fceaf6f20348d3e4cadad7a3e9f34
abcyeji <fndl0201@naver.com>
2025-07-13 23:01:41 +0900

    250713집에서 코딩

--- p_jongro/js/main.js
+++ p_jongro/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+
+
+
+
+/**************visual 시작************** */
             const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
             // autoplay: {  /* 팝업 자동 실행 */
@@ -21,6 +26,28 @@ $(document).ready(function(){
             },
             
         });
+/***************tourist 시작**************************/
+                // 1. 클릭한 li에서 data-content값을 가져와서
+                //    ==>tab_item 중에 해당값이 id인 요소를 찾아서 나타나게 해야함 (다른 요소는 숨김)
+                // 2. 클릭한 li에만 active 클래스 줌
+                // 3.클릭한 li안에 있는 span에 선택됨이라고 글자 써줌( 다른 li에 있는건 삭제)
+                // 4.클릭한 li속성 aria-selected값을 true로 변경 (다른 li는 모두 false)
+
+                let tourist_content //클릭한 메뉴의 이름(id)
+                $('.tourist .inner .tap_list ul li').on('click', function(){
+
+                    
+                    if($(this).hasClass('active') == false){
+                        // console.log('선택안된메뉴...')
+                        tourist_content = $(this).attr('data-content')//tab-item1
+                        // console.log(find_content)
+                        $('.tourist .tab_content .tab_item').removeClass('active')
+                        $('.tourist .tab_content').find('#'+tourist_content).addClass('active')
+                        
+                        $('.tourist .inner .tap_list ul li').removeClass('active')
+                        $(this).addClass('active')
+                    }
+                })
 
 
 /******************notice시작**************************** */
