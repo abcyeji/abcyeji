c0c6d037f13aa9a8e3fcb5515d64c255bd2adc88
abcyeji <fndl0201@naver.com>
2025-07-24 16:01:12 +0900

    250724 학원에서 코딩

--- startpage/js/main.js
+++ startpage/js/main.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 
 		const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
 
-			navigation: true, /* 오른쪽에 각 페이지의 paging */
-			navigationPosition: 'right', /* 위치 */
-			navigationTooltips: ['첫번째', '두번째', '세번째', '네번째'], /* 툴팁 */
+			navigation: false, /* 오른쪽에 각 페이지의 paging */
+			navigationPosition: 'left', /* 위치 */
+			navigationTooltips: ['home', 'proflie', 'project1', 'project2'], /* 툴팁 */
 			showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
 			
-			lockAnchors: true,
-			anchors: ['link1', 'link2', 'link3', 'link4'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
+			lockAnchors: false,
+			anchors: ['home', 'proflie', 'project1', 'project2'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
 
 			autoScrolling:true, /* 한페이지씩 스크롤 */
 			scrollHorizontally: true,
@@ -19,15 +19,34 @@ $(document).ready(function(){
 			scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
 
 			afterLoad: function(origin, destination, direction, trigger){
-				if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-					console.log('3번째 슬라이드가 로딩 되었을때');
-				}
+				$('header .gnb ul li').removeClass('active')
+				$('header .gnb ul li').eq(destination.index).addClass('active')
 			},
 
 			responsiveWidth: 640 /* fullpage를 적용시키지 않을 모바일 사이즈 */
 		});
 		
-
+/*************************************** */
+		// const snbScroll = function() {
+		// 	const $menu_wrap = $("header .gnb ul");  /* 선택자를 잘 입력해야함 */
+		// 	const $menu_li = $("header .gnb ul li");  
+		// 	function scrollToElement($element) {
+		// 		const containerWidth = $menu_wrap.width();
+		// 		const itemWidth = $element.outerWidth(true);
+		// 		const totalItemsWidth = $menu_wrap[0].scrollWidth;
+		// 		const newScrollPosition = ($element.index() === 0) ? 0 :
+		// 			($element.index() === $menu_li.length - 1) ? totalItemsWidth - containerWidth :
+		// 			$element.position().left + $menu_wrap.scrollLeft() - (containerWidth - itemWidth) / 2;
+		// 		$menu_wrap.animate({
+		// 			scrollLeft: newScrollPosition
+		// 		}, 500);
+		// 	}
+		// 	const $activeItem = $menu_wrap.find(".active");
+		// 	if ($activeItem.length) {
+		// 		scrollToElement($activeItem);
+		// 	}
+		// } 
+		// snbScroll();   /* 함수의 실행 */
 
 
 
